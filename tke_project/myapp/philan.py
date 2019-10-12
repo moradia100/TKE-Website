9c757c72216efa055a7c60024d5cfa59eb988b21
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-10-12 15:13:30 -0700

    Philanthropy path created

---
+++ tke_project/myapp/philan.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    context = {
+        "variable":"Philanthropy",
+        "title":"TKE",
+    }
+    return render(request, "index.html", context=context)
--- tke_project/myapp/urls.py
+++ tke_project/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from . import philan
 
 urlpatterns = [
 		path('', views.index),
+		path('philanthropy/', philan.index)
 ]
